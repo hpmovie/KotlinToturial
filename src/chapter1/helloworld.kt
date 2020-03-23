352e3a7a9ab829e836af24169ec7186dde72eb8f
hpmovie <you@example.com>
2020-03-23 12:06:36 +0900

    자동형변환 불가능

--- src/chapter1/helloworld.kt
+++ src/chapter1/helloworld.kt
@@ -5,7 +5,7 @@ fun main() {
     val data2 = "hello world"
 
     var data3:Float = 0.1F
-    var data4:Double = data3.toDouble()
+    var data4:Double = data3.toDouble() // 자동 형변환 불가능
 
     println(data2)
 }
